c73a17e2fde90de64523c5cc1ebcb8611f8f66fe
Jcardster <10875523@my.uvu.edu>
2023-10-25 16:25:09 -0600

    Midterm Sorting Method
    Using ChatGPT Created a program to quicksort customers based off of scores

---
+++ BarrJustinSodaMixMidterm/BarrJustinSodaMixMidterm/Program.cs
@@ -0,0 +1,45 @@
+﻿using BarrJustinSodaMixMidterm;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Create an array of customers from an instance of the customer class
+        Customer[] customers = {
+            new Customer("Jim", 34),
+            new Customer("Anne", 71),
+            new Customer("Paul", 14),
+            new Customer("Kodey", 99),
+            new Customer("Kameron", 81),
+            new Customer("Lisa", 45),
+            new Customer("Jan", 5)
+        };
+
+        //Call the QuickSort Class and sort the customers
+        QuickSort.Sort(customers);
+
+        //Print to the Console the results
+        Console.WriteLine("\nSorted Customers (by Score in Descending Order):");
+        PrintCustomers(customers);
+
+        //Make the results visible until the user is done
+        Console.WriteLine("\nPress enter to exit");
+        Console.ReadLine();
+    }
+
+    /// <summary>
+    /// Prints the customers in a formatted line
+    /// </summary>
+    /// <param name="customers"></param>
+    static void PrintCustomers(Customer[] customers)
+    {
+        //Loop through every customer in the array
+        foreach (var customer in customers)
+        {
+            //Write the customer name and score
+            Console.WriteLine($"Name: {customer.Name}, Score: {customer.Score}");
+        }
+    }
+}
+
---
+++ BarrJustinSodaMixMidterm/BarrJustinSodaMixMidterm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BarrJustinSodaMixMidterm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BarrJustinSodaMixMidterm")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("77fb25fe-2b74-4aa8-a422-27a0d52ee98c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
